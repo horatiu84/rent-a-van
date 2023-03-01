7d16cb06a66a58230166fe20304639634463e12f
horatiu84 <horatiul@yahoo.com>
2023-03-01 06:24:49 +0200

    fixed warning

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <>
       <nav className="navbar">
-        <Link className="home_link" to="/">{<img src={LogoVan} />}</Link>
+        <Link className="home_link" to="/">{<img src={LogoVan} alt="the logo for the app"/>}</Link>
         <div className="links">
           <Link to="/about">About</Link>
           <Link to="/vans">Vans</Link>
