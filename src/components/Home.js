7141bb11bdb3c1c8a03cdd633accdd71518b9431
horatiu84 <horatiul@yahoo.com>
2023-03-01 06:10:09 +0200

    added style for about component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Home.css';
-import Mountains from '../assets/image 54.png'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className='home_div'>
         <h2>You got the travel plans, we got the travel vans.</h2>
         <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-      <div className='find_van'>Find your van</div>
+      <div className='find_van'> <Link  to='/vans'>Find your van</Link></div>
 
     </div>
   )
